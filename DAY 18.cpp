743ce541564939d62d371ff8df0e5faa67f1b5a2
shivani1234singh <justmeshivi@gmail.com>
2026-02-25 13:01:08 +0530

    Add files via upload

---
+++ DAY 18.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[n];
+
+    for(int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", temp[i]);
+    }
+
+    return 0;
+}
