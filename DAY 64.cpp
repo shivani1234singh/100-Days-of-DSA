aff5929b5457bf23aede6676d863c071e73f8b39
shivani1234singh <justmeshivi@gmail.com>
2026-04-10 21:23:09 +0530

    Add files via upload

--- DAY 64.cpp
+++ DAY 64.cpp
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("hellon");
+    int n;
+    scanf("%d", &n);
+
+    int **adj = (int **)malloc(n * sizeof(int *));
+    int *size = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int k;
+        scanf("%d", &k);
+        size[i] = k;
+        adj[i] = (int *)malloc(k * sizeof(int));
+        for (int j = 0; j < k; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    int s;
+    scanf("%d", &s);
+
+    int *visited = (int *)calloc(n, sizeof(int));
+    int *queue = (int *)malloc(n * sizeof(int));
+    int front = 0, rear = 0;
+
+    visited[s] = 1;
+    queue[rear++] = s;
+
+    while (front < rear) {
+        int v = queue[front++];
+        printf("%d ", v);
+
+        for (int i = 0; i < size[v]; i++) {
+            int u = adj[v][i];
+            if (!visited[u]) {
+                visited[u] = 1;
+                queue[rear++] = u;
+            }
+        }
+    }
+
     return 0;
 }
